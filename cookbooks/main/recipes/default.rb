acff0b2ebf9729b3603437433c0c057ace622593
ayano-opendna <ayano@open-dna.jp>
2014-04-22 19:07:30 +0900

    octopus対応

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,3 +1,5 @@
+require_recipe "octopus"
+
 #execute "testing" do
 #  command %Q{
 #    echo "i ran at #{Time.now}" >> /root/cheftime
---
+++ cookbooks/octopus/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: octopus
+# Recipe:: default
+#
+
+if ['solo', 'app_master', 'app', 'util'].include?(node[:instance_role])
+  # for each application
+  node[:engineyard][:environment][:apps].each do |app|
+
+    # create new shards.yml for octopus
+    template "/data/#{app[:name]}/shared/config/shards.yml" do
+      source 'shards.yml.erb'
+      owner node[:users][0][:username]
+      group node[:users][0][:username]
+      mode 0644
+      variables({
+        :environment => node[:environment][:framework_env],
+        :adapter => 'mysql2',
+        :database => app[:database_name],
+        :username => node[:users][0][:username],
+        :password => node[:users][0][:password],
+        :host => node[:db_host],
+        :slaves => node.environment.instances.select{|i| i["role"] =="db_slave"},
+      })
+    end
+    command "ln -sfv /data/#{app[:name]}/shared/config/shards.yml #{current_path}/config/shards.yml"
+  end
+end
+
+ey_cloud_report "nginx" do 
+  message "create file, #{current_path}/config/shards.yml" 
+end
