5a2d88b172de34bd17951d916e02adda511333b5
ayano-opendna <ayano@open-dna.jp>
2014-04-23 12:21:45 +0900

    fix

--- cookbooks/octopus/recipes/default.rb
+++ cookbooks/octopus/recipes/default.rb
@@ -23,7 +23,7 @@ if ['solo', 'app_master', 'app', 'util'].include?(node[:instance_role])
         :username => node[:users][0][:username],
         :password => node[:users][0][:password],
         :host => node[:db_host],
-        :slaves => node[:engineyard][:environment][:instances].find{|i| i[:role][/db_slave/]}
+        :slaves => node[:engineyard][:environment][:instances].select{|i| i["role"] =="db_slave"},
       })
     end
   end
