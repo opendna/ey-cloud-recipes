f12fc71ae6713620015b0c3468a04793bec2b0c4
ayano-opendna <ayano@open-dna.jp>
2014-05-13 16:19:38 +0900

    SSLのkey/crtが無い場合は、confを削除する

--- cookbooks/ssl_config/recipes/default.rb
+++ cookbooks/ssl_config/recipes/default.rb
@@ -10,22 +10,26 @@ if ['solo', 'app_master', 'app', 'util'].include?(node[:instance_role])
   # for each application
   node[:engineyard][:environment][:apps].each do |app|
     
-    next unless File.exist? "/data/nginx/ssl/#{app.name}.crt"
-    next unless File.exist? "/data/nginx/ssl/#{app.name}.key"
-
-    # create /data/nginx/servers/#{app.name}.ssl.custom.conf
-    template "/data/nginx/servers/#{app[:name]}.ssl.custom.conf" do
-      source 'ssl.conf.erb'
-      owner node[:users][0][:username]
-      group node[:users][0][:username]
-      mode 0644
-      variables({
-        :node => node,
-        :https_bind_port => nginx_https_port,
-        :server_names => app[:vhosts][0][:domain_name].empty? ? [] : [app[:vhosts][0][:domain_name]],
-        :app_name => app[:name],
-        :app_type => app[:app_type],
-      })
+    if File.exist?("/data/nginx/ssl/#{app.name}.crt") && File.exist?("/data/nginx/ssl/#{app.name}.key")
+      # create /data/nginx/servers/#{app.name}.ssl.custom.conf
+      conf_file = "/data/nginx/servers/#{app[:name]}.ssl.custom.conf"
+      template conf_file do
+        source 'ssl.conf.erb'
+        owner node[:users][0][:username]
+        group node[:users][0][:username]
+        mode 0644
+        variables({
+          :node => node,
+          :https_bind_port => nginx_https_port,
+          :server_names => app[:vhosts][0][:domain_name].empty? ? [] : [app[:vhosts][0][:domain_name]],
+          :app_name => app[:name],
+          :app_type => app[:app_type],
+        })
+      end
+    else
+      if File.exist?(conf_file)
+        File.delete conf_file
+      end
     end
   end
 end
