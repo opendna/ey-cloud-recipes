0a2a834f6b0af19ee38ba75303a3169704c9af34
ayano-opendna <ayano@open-dna.jp>
2014-07-30 10:40:05 +0900

    shars.ymlがあるばあいは、上書きしないように修正

--- cookbooks/octopus/recipes/default.rb
+++ cookbooks/octopus/recipes/default.rb
@@ -7,21 +7,25 @@ if ['solo', 'app_master', 'app', 'util'].include?(node[:instance_role])
   # for each application
   node[:engineyard][:environment][:apps].each do |app|
 
-    # create new shards.yml for octopus
-    template "/data/#{app[:name]}/shared/config/shards.yml" do
-      source 'shards.yml.erb'
-      owner node[:users][0][:username]
-      group node[:users][0][:username]
-      mode 0644
-      variables({
-        :environment => node[:environment][:framework_env],
-        :adapter => 'mysql2',
-        :database => app[:database_name],
-        :username => node[:users][0][:username],
-        :password => node[:users][0][:password],
-        :host => node[:db_host],
-        :slaves => node[:engineyard][:environment][:instances].select{|i| i["role"] =="db_slave"},
-      })
+    conf_file = "/data/#{app[:name]}/shared/config/shards.yml"
+
+    unless File.exist? conf_file
+      # create new shards.yml for octopus
+      template conf_file do
+        source 'shards.yml.erb'
+        owner node[:users][0][:username]
+        group node[:users][0][:username]
+        mode 0644
+        variables({
+          :environment => node[:environment][:framework_env],
+          :adapter => 'mysql2',
+          :database => app[:database_name],
+          :username => node[:users][0][:username],
+          :password => node[:users][0][:password],
+          :host => node[:db_host],
+          :slaves => node[:engineyard][:environment][:instances].select{|i| i["role"] =="db_slave"},
+        })
+      end
     end
   end
 end
