8a16802bfd7734f3bd804453c7951478858565f6
yoshikazu-dna <yoshikazu@open-dna.jp>
2020-05-19 11:52:05 +0900

    テスト

--- cookbooks/database/recipes/default.rb
+++ cookbooks/database/recipes/default.rb
@@ -9,19 +9,35 @@ if ['solo', 'app_master', 'app', 'util'].include?(node[:instance_role])
  
     # create new database.yml
     template "/data/#{app[:name]}/shared/config/database.yml" do
-      source 'database.yml.erb'
-      owner node[:users][0][:username]
-      group node[:users][0][:username]
-      mode 0644
-      variables({
-        :environment => node[:environment][:framework_env],
-        :adapter => 'mysql2',
-        :database => app[:database_name],
-        :username => node[:users][0][:username],
-        :password => node[:users][0][:password],
-        :host => node[:db_host],
-        :slaves => node[:engineyard][:environment][:instances].select{|i| i["role"] =="db_slave"},
-      })
+      if ['loanrank'].include?(app[:name])
+        source 'aws_database.yml.erb'
+        owner node[:users][0][:username]
+        group node[:users][0][:username]
+        mode 0644
+        variables({
+                    :environment => node[:environment][:framework_env],
+                    :adapter => 'mysql2',
+                    :database => app[:database_name],
+                    :username => node[:users][0][:username],
+                    :password => node[:users][0][:password],
+                    :host => node[:db_host],
+                    :slaves => node[:engineyard][:environment][:instances].select{|i| i["role"] =="db_slave"},
+                  })
+      else
+        source 'database.yml.erb'
+        owner node[:users][0][:username]
+        group node[:users][0][:username]
+        mode 0644
+        variables({
+                    :environment => node[:environment][:framework_env],
+                    :adapter => 'mysql2',
+                    :database => app[:database_name],
+                    :username => node[:users][0][:username],
+                    :password => node[:users][0][:password],
+                    :host => node[:db_host],
+                    :slaves => node[:engineyard][:environment][:instances].select{|i| i["role"] =="db_slave"},
+                  })
+      end
     end
   end
 end
